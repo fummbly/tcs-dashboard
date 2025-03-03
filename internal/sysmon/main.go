2cde45586fb2edfeea522fbb01f72b1cc93a643c
Tom <tstone449@gmail.com>
2025-03-03 17:05:47 -0500

    getting internal sysmon setup

---
+++ internal/sysmon/main.go
@@ -0,0 +1,3 @@
+package sysmon
+
+import ()
--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/shirou/gopsutil/v4/sys"
-)
+import ()
 
 func main() {
 
-	for {
-		getUsage()
-		time.Sleep(4 * time.Second)
-	}
-
-}
-
-func getUsage() {
-	v, _ :=
-		fmt.Printf("Total: %v, Free: %v,  UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
 }
